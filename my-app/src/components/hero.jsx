68b0a50c26ab9ede61ff01979c6403fd579e9b53
eashirokovaa <eashirokovaa@gmail.com>
2023-01-30 21:39:01 +0300

    Homework heroes

---
+++ my-app/src/components/hero.jsx
@@ -0,0 +1,15 @@
+import "./hero.css"
+
+function Hero(props) {
+    return (
+      <div className="hero-card">
+        <h2 className="hero-name">{props.name}</h2>
+        <div className="hero-universe"><b>Вселенная:</b> {props.universe}</div>
+        <div className="hero-superpower"><b>Суперсила:</b> {props.superpower}</div>
+        <img src={props.url}/>
+      </div>
+    );
+  }
+  
+  export default Hero;
+  
