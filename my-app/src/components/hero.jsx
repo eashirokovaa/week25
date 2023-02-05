d2444e10173e632d633d2c254c8f539354639faf
eashirokovaa <eashirokovaa@gmail.com>
2023-02-05 18:51:47 +0300

    Heroes hw

--- my-app/src/components/hero.jsx
+++ my-app/src/components/hero.jsx
@@ -1,15 +1,18 @@
-import "./hero.css"
+import "/Users/katasirokova/week25/my-app/src/style/hero.css";
 
 function Hero(props) {
-    return (
-      <div className="hero-card">
-        <h2 className="hero-name">{props.name}</h2>
-        <div className="hero-universe"><b>Вселенная:</b> {props.universe}</div>
-        <div className="hero-superpower"><b>Суперсила:</b> {props.superpower}</div>
-        <img src={props.url}/>
+  return (
+    <div className="hero-card">
+      <h2 className="hero-name">{props.name}</h2>
+      <div className="hero-universe">
+        <b>Вселенная:</b> {props.universe}
       </div>
-    );
-  }
-  
-  export default Hero;
-  
+      <div className="hero-superpower">
+        <b>Суперсила:</b> {props.superpower}
+      </div>
+      <img src={props.url} />
+    </div>
+  );
+}
+
+export default Hero;
